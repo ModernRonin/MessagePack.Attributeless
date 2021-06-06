02ca5c4475eb5950feed34e3e3951bb1cdee791b
Mark Hajek <markhajek@outlook.com>
2021-06-06 17:44:57 +0100

    added NullFormatter<>

---
+++ MessagePack.Attributeless/NullFormatter.cs
@@ -0,0 +1,11 @@
+﻿using MessagePack.Formatters;
+
+namespace MessagePack.Attributeless
+{
+    public class NullFormatter<T> : IMessagePackFormatter<T>
+    {
+        public T Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options) => default;
+
+        public void Serialize(ref MessagePackWriter writer, T value, MessagePackSerializerOptions options) { }
+    }
+}
