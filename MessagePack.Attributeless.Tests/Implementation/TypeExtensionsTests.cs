23c158b52eb5da0c51ffb1833eb6c4f2c8be8b0d
Mark Hajek <markhajek@outlook.com>
2022-01-17 12:25:24 +0000

    updated nugets packages and adjusted to changes

--- MessagePack.Attributeless.Tests/Implementation/TypeExtensionsTests.cs
+++ MessagePack.Attributeless.Tests/Implementation/TypeExtensionsTests.cs
@@ -5,176 +5,206 @@ using FluentAssertions;
 using MessagePack.Attributeless.Implementation;
 using NUnit.Framework;
 
-namespace MessagePack.Attributeless.Tests.Implementation
+namespace MessagePack.Attributeless.Tests.Implementation;
+
+[TestFixture]
+public class TypeExtensionsTests
 {
-    [TestFixture]
-    public class TypeExtensionsTests
+    public class Generic<T> { }
+
+    public class Element { }
+
+    public class UserOfGenericWithTypeParameterInAssembly
     {
-        public class Generic<T> { }
+        public Generic<Element> Elements { get; set; }
+    }
 
-        public class Element { }
+    public class UserOfGenericWithTypeParameterOutsideAssembly
+    {
+        public Generic<string> Elements { get; set; }
+    }
 
-        public class UserOfGenericWithTypeParameterInAssembly
+    public class Node
+    {
+        public ICollection<Node> Children { get; set; }
+    }
+
+    class TypeUsingExternalGeneric
+    {
+        public List<Element> Elements { get; set; }
+    }
+
+    class ContainsNullable
+    {
+        public int? Count { get; set; }
+        public Samples.Side? Side { get; set; }
+    }
+
+    class HasIndexer
+    {
+        readonly Dictionary<string, Element> _map = new();
+
+        public Element this[string key]
         {
-            public Generic<Element> Elements { get; set; }
+            get => _map[key];
+            set => _map[key] = value;
         }
 
-        public class UserOfGenericWithTypeParameterOutsideAssembly
-        {
-            public Generic<string> Elements { get; set; }
-        }
+        public ICollection<Node> Nodes { get; set; }
+    }
 
-        public class Node
-        {
-            public ICollection<Node> Children { get; set; }
-        }
-
-        class TypeUsingExternalGeneric
-        {
-            public List<Element> Elements { get; set; }
-        }
-
-        class ContainsNullable
-        {
-            public int? Count { get; set; }
-            public Samples.Side? Side { get; set; }
-        }
-
-        class HasIndexer
-        {
-            readonly Dictionary<string, Element> _map = new();
-
-            public Element this[string key]
+    [Test]
+    public void GetReferencedUserTypes_deals_correctly_with_nullables()
+    {
+        typeof(ContainsNullable).GetReferencedUserTypes()
+            .Should()
+            .BeEquivalentTo(new[]
             {
-                get => _map[key];
-                set => _map[key] = value;
-            }
+                typeof(ContainsNullable),
+                typeof(Samples.Side)
+            });
+    }
 
-            public ICollection<Node> Nodes { get; set; }
-        }
-
-        [Test]
-        public void GetReferencedUserTypes_deals_correctly_with_nullables()
-        {
-            typeof(ContainsNullable).GetReferencedUserTypes()
-                .Should()
-                .BeEquivalentTo(typeof(ContainsNullable), typeof(Samples.Side));
-        }
-
-        [Test]
-        public void GetReferencedUserTypes_does_not_include_indexed_properties()
-        {
-            typeof(HasIndexer).GetReferencedUserTypes()
-                .Should()
-                .BeEquivalentTo(typeof(HasIndexer), typeof(Node));
-        }
-
-        [Test]
-        public void GetReferencedUserTypes_with_abstract_class()
-        {
-            var type = typeof(Samples.AnExtremity);
-            var expected = new[]
+    [Test]
+    public void GetReferencedUserTypes_does_not_include_indexed_properties()
+    {
+        typeof(HasIndexer).GetReferencedUserTypes()
+            .Should()
+            .BeEquivalentTo(new[]
             {
-                type,
-                typeof(Samples.Arm),
-                typeof(Samples.Leg),
-                typeof(Samples.Wing)
-            };
+                typeof(HasIndexer),
+                typeof(Node)
+            });
+    }
 
-            type.GetReferencedUserTypes().Should().BeEquivalentTo(expected);
-        }
-
-        [Test]
-        public void GetReferencedUserTypes_with_complex_graph()
+    [Test]
+    public void GetReferencedUserTypes_with_abstract_class()
+    {
+        var type = typeof(Samples.AnExtremity);
+        var expected = new[]
         {
-            var type = typeof(Samples.PersonWithPet);
+            type,
+            typeof(Samples.Arm),
+            typeof(Samples.Leg),
+            typeof(Samples.Wing)
+        };
 
-            var actual = type.GetReferencedUserTypes();
+        type.GetReferencedUserTypes().Should().BeEquivalentTo(expected);
+    }
 
-            var asText = string.Join('\n', actual.OrderBy(t => t.Name).Select(t => t.Name));
-            Approvals.Verify(asText);
-        }
+    [Test]
+    public void GetReferencedUserTypes_with_complex_graph()
+    {
+        var type = typeof(Samples.PersonWithPet);
 
-        [Test]
-        public void GetReferencedUserTypes_with_external_generic_doesnt_include_that()
-        {
-            var type = typeof(TypeUsingExternalGeneric);
+        var actual = type.GetReferencedUserTypes();
 
-            type.GetReferencedUserTypes().Should().BeEquivalentTo(type, typeof(Element));
-        }
+        var asText = string.Join('\n', actual.OrderBy(t => t.Name).Select(t => t.Name));
+        Approvals.Verify(asText);
+    }
 
-        [Test]
-        public void GetReferencedUserTypes_with_generic_type_with_type_parameter_in_assembly()
-        {
-            var type = typeof(UserOfGenericWithTypeParameterInAssembly);
+    [Test]
+    public void GetReferencedUserTypes_with_external_generic_doesnt_include_that()
+    {
+        var type = typeof(TypeUsingExternalGeneric);
 
-            type.GetReferencedUserTypes()
-                .Should()
-                .BeEquivalentTo(type, typeof(Element), typeof(Generic<Element>));
-        }
-
-        [Test]
-        public void GetReferencedUserTypes_with_generic_type_with_type_parameter_outside_assembly()
-        {
-            var type = typeof(UserOfGenericWithTypeParameterOutsideAssembly);
-
-            type.GetReferencedUserTypes().Should().BeEquivalentTo(type, typeof(Generic<string>));
-        }
-
-        [Test]
-        public void GetReferencedUserTypes_with_interface()
-        {
-            var type = typeof(Samples.IExtremity);
-            var expected = new[]
+        type.GetReferencedUserTypes()
+            .Should()
+            .BeEquivalentTo(new[]
             {
                 type,
-                typeof(Samples.Arm),
-                typeof(Samples.Leg),
-                typeof(Samples.Wing)
-            };
+                typeof(Element)
+            });
+    }
 
-            type.GetReferencedUserTypes().Should().BeEquivalentTo(expected);
-        }
+    [Test]
+    public void GetReferencedUserTypes_with_generic_type_with_type_parameter_in_assembly()
+    {
+        var type = typeof(UserOfGenericWithTypeParameterInAssembly);
 
-        [Test]
-        public void GetReferencedUserTypes_with_nested_interface()
-        {
-            var type = typeof(Samples.IAnimal);
-            var expected = new[]
+        type.GetReferencedUserTypes()
+            .Should()
+            .BeEquivalentTo(new[]
             {
                 type,
-                typeof(Samples.IExtremity),
-                typeof(Samples.Arm),
-                typeof(Samples.Leg),
-                typeof(Samples.Wing),
-                typeof(Samples.Bird),
-                typeof(Samples.Mammal)
-            };
+                typeof(Element),
+                typeof(Generic<Element>)
+            });
+    }
 
-            type.GetReferencedUserTypes().Should().BeEquivalentTo(expected);
-        }
+    [Test]
+    public void GetReferencedUserTypes_with_generic_type_with_type_parameter_outside_assembly()
+    {
+        var type = typeof(UserOfGenericWithTypeParameterOutsideAssembly);
 
-        [Test]
-        public void GetReferencedUserTypes_with_only_non_user_types()
+        type.GetReferencedUserTypes()
+            .Should()
+            .BeEquivalentTo(new[]
+            {
+                type,
+                typeof(Generic<string>)
+            });
+    }
+
+    [Test]
+    public void GetReferencedUserTypes_with_interface()
+    {
+        var type = typeof(Samples.IExtremity);
+        var expected = new[]
         {
-            var type = typeof(Samples.Address);
+            type,
+            typeof(Samples.Arm),
+            typeof(Samples.Leg),
+            typeof(Samples.Wing)
+        };
 
-            type.GetReferencedUserTypes().Should().BeEquivalentTo(type);
-        }
+        type.GetReferencedUserTypes().Should().BeEquivalentTo(expected);
+    }
 
-        [Test]
-        public void GetReferencedUserTypes_with_other_type_inside_collection()
+    [Test]
+    public void GetReferencedUserTypes_with_nested_interface()
+    {
+        var type = typeof(Samples.IAnimal);
+        var expected = new[]
         {
-            var type = typeof(Samples.Person);
+            type,
+            typeof(Samples.IExtremity),
+            typeof(Samples.Arm),
+            typeof(Samples.Leg),
+            typeof(Samples.Wing),
+            typeof(Samples.Bird),
+            typeof(Samples.Mammal)
+        };
 
-            type.GetReferencedUserTypes().Should().BeEquivalentTo(type, typeof(Samples.Address));
-        }
+        type.GetReferencedUserTypes().Should().BeEquivalentTo(expected);
+    }
 
-        [Test]
-        [Timeout(500)]
-        public void GetReferencedUserTypes_with_self_referential_structure()
-        {
-            typeof(Node).GetReferencedUserTypes().Should().Equal(typeof(Node));
-        }
+    [Test]
+    public void GetReferencedUserTypes_with_only_non_user_types()
+    {
+        var type = typeof(Samples.Address);
+
+        type.GetReferencedUserTypes().Should().BeEquivalentTo(new[] { type });
+    }
+
+    [Test]
+    public void GetReferencedUserTypes_with_other_type_inside_collection()
+    {
+        var type = typeof(Samples.Person);
+
+        type.GetReferencedUserTypes()
+            .Should()
+            .BeEquivalentTo(new[]
+            {
+                type,
+                typeof(Samples.Address)
+            });
+    }
+
+    [Test]
+    [Timeout(500)]
+    public void GetReferencedUserTypes_with_self_referential_structure()
+    {
+        typeof(Node).GetReferencedUserTypes().Should().Equal(typeof(Node));
     }
 }
