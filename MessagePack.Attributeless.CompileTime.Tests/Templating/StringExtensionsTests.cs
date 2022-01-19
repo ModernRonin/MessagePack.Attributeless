ec51f64ee4d70f03c04669c3724b32c1b52a13b7
Mark Hajek <markhajek@outlook.com>
2022-01-19 19:00:55 +0000

    test coverage for StringExtensions

---
+++ MessagePack.Attributeless.CompileTime.Tests/Templating/StringExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using MessagePack.Attributeless.CompileTime.Templating;
+using NUnit.Framework;
+
+namespace MessagePack.Attributeless.CompileTime.Tests.Templating;
+
+[TestFixture]
+public class StringExtensionsTests
+{
+    [TestCase("hello world", "hello ", "world")]
+    [TestCase("hello world", "wo", "rld")]
+    [TestCase("hello world", "wx", "hello world")]
+    [TestCase("", "wx", "")]
+    [TestCase("hello", "", "hello")]
+    public void After(string input, string pattern, string expected) =>
+        input.After(pattern).Should().Be(expected);
+
+    [TestCase("hello world", "hello ", "")]
+    [TestCase("hello world", "wo", "hello ")]
+    [TestCase("hello world", "wx", "hello world")]
+    [TestCase("", "wx", "")]
+    [TestCase("hello", "", "hello")]
+    public void Before(string input, string pattern, string expected) =>
+        input.Before(pattern).Should().Be(expected);
+}
